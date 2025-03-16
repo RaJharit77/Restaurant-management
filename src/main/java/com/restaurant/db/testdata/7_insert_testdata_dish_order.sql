51434eac904537149f4fa8c465c4e148239a0932
RaJharit77 <hei.rajo.2@gmail.com>
2025-03-16 13:28:23 +0300

    feat: Update insert_testdata_dish_order.sql, insert_testdata_order_status.sql, insert_testdata_dish_order_status.sql files

--- src/main/java/com/restaurant/db/testdata/7_insert_testdata_dish_order.sql
+++ src/main/java/com/restaurant/db/testdata/7_insert_testdata_dish_order.sql
@@ -1,7 +1,7 @@
 INSERT INTO Dish_Order (order_id, dish_id, quantity, status)
-VALUES (1, 1, 2, 'CREATED'),   -- ORDER-001 : 2 Hot Dogs
-       (1, 2, 1, 'CREATED'),   -- ORDER-001 : 1 Cheeseburger
-       (2, 3, 1, 'CONFIRMED'), -- ORDER-002 : 1 Hamburger
-       (2, 4, 1, 'CONFIRMED'), -- ORDER-002 : 1 Salade
+VALUES (1, 1, 2, 'CREATED'),
+       (1, 2, 1, 'CREATED'),
+       (2, 3, 1, 'CONFIRMED'),
+       (2, 4, 1, 'CONFIRMED'),
        (3, 1, 3, 'IN_PREPARATION')
-on conflict do nothing; -- ORDER-003 : 3 Hot Dogs
+on conflict do nothing;
--- src/main/java/com/restaurant/db/testdata/9_insert_testdata_dish_order_status.sql
+++ src/main/java/com/restaurant/db/testdata/9_insert_testdata_dish_order_status.sql
@@ -1,8 +1,8 @@
-INSERT INTO Status_History (dish_order_id, status, changed_at)
-VALUES (1, 'CREATED', '2025-03-01 10:00:00'),        -- Dish_Order 1 : CREATED
-       (1, 'CONFIRMED', '2025-03-01 10:05:00'),      -- Dish_Order 1 : CONFIRMED
-       (2, 'CREATED', '2025-03-01 10:00:00'),        -- Dish_Order 2 : CREATED
-       (2, 'IN_PREPARATION', '2025-03-01 10:10:00'), -- Dish_Order 2 : IN_PREPARATION
-       (3, 'CONFIRMED', '2025-03-01 10:15:00'),      -- Dish_Order 3 : CONFIRMED
+INSERT INTO Dish_Order_Status (dish_order_status_id, status, changed_at)
+VALUES (1, 'CREATED', '2025-03-01 10:00:00'),
+       (1, 'CONFIRMED', '2025-03-01 10:05:00'),
+       (2, 'CREATED', '2025-03-01 10:00:00'),
+       (2, 'IN_PREPARATION', '2025-03-01 10:10:00'),
+       (3, 'CONFIRMED', '2025-03-01 10:15:00'),
        (3, 'COMPLETED', '2025-03-01 10:30:00')
-on conflict do nothing; -- Dish_Order 3 : COMPLETED
+on conflict do nothing;
